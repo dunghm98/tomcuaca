937f8b1e1bd654032896259f3cd6e08e1200e9b8
Dung Hoang Manh <dunghm@bsscommerce.com>
2023-01-23 19:45:57 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
+import classNames from "classnames";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const dices = ["calabash", "crab", "deer", "fish", "rooster", "shrimp"];
+
+  const getRandomList = () => {
+    return dices.sort(() => 0.5 - Math.random()).slice(0, 3);
+  };
+
+  const [randomList, setRandomList] = useState(getRandomList());
+  const [vibrate, setVibrate] = useState(false);
+
+  console.log(randomList);
+
+  const handleRoll = () => {
+    setVibrate(true);
+    play();
+    setTimeout(() => {
+      setVibrate(false);
+      setRandomList(getRandomList());
+    }, 4000);
+  };
+
+  function play() {
+    var audio = document.getElementById("a1");
+    audio.play();
+  }
+
+
   return (
     <>
       <Head>
@@ -14,110 +41,44 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
+      <main className={'main'}>
+        <div className={classNames("bau_cua_wrapper", { open: open })}>
           <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
+            // className={"dia_co_tien"}
+            className={classNames("dia_co_tien", { shake: vibrate })}
+            src="/dia-co-tien.png"
+            alt="Dia co tien"
+            width={500}
+            height={500}
             priority
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <div className="dice-result-wrapper">
+            {randomList.map((item) => {
+              return (
+                <Image
+                  className={"result-dice"}
+                  src={"/tomcuaca/" + item + ".svg"}
+                  alt="Result"
+                  width={100}
+                  height={100}
+                  priority
+                />
+              );
+            })}
           </div>
+          <Image
+            className={classNames("bat_sat", { shake: vibrate })}
+            src="/bat-su.png"
+            alt="Bat sat"
+            width={400}
+            height={400}
+            priority
+          />
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <button onClick={() => setOpen(!open)}>{!open ? "Mở" : "Đóng"}</button>
+        <button onClick={handleRoll}>Xóc</button>
+        <audio id="a1" src="/shake-dice.mp3" />
       </main>
     </>
-  )
+  );
 }
